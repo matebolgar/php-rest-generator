b855c2b96364c1eb195fc09dac2f0862788deb5f
Máté <bolgar.m@gmail.com>
2021-04-26 10:49:21 +0200

    fix

--- generator-process/src/root/index.ts
+++ generator-process/src/root/index.ts
@@ -80,13 +80,11 @@ if ($_SERVER['DEPLOYMENT_ENV'] === 'dev') {
     mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 }
 
-if ($_SERVER['REQUEST_METHOD'] === "OPTIONS") {
-    header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN'] ?? '*');
-    header("Access-Control-Allow-Credentials: true");
-    header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS");
-    header('Access-Control-Allow-Headers: Origin, Pragma, Cache-control, X-Requested-With, Content-Type, Accept, Authorization');
-    exit;
-}
+header("Access-Control-Allow-Origin: " . ($_SERVER['HTTP_ORIGIN'] ?? '*'));
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS");
+header('Access-Control-Allow-Headers: Origin, Pragma, Cache-control, X-Requested-With, Content-Type, Accept, Authorization');
+
 
 
 $conn = new mysqli(
